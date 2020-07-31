08edfda1c5094e90254ee6eeb53e2b74bed0b5f4
SidtheKid0504 <63013935+SidtheKid0504@users.noreply.github.com>
2020-07-31 13:01:24 -0400

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,75 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+    car1 = createSprite(100, 200);
+    car2 = createSprite(300, 200);
+    car3 = createSprite(500, 200);
+    car4 = createSprite(700, 200);
+
+    cars = [car1, car2, car3, car4];
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      
+      // Index of Cars Array
+      var index = 0;
+
+      var posX = 0;
+      var posY;
+
+      for(var plr in allPlayers){
+        index++;
+        posX += 200;
+        posY = displayHeight - allPlayers[plr].distance;
+        cars[index - 1].x = posX;
+        cars[index - 1].y = posY;
+
+        if (index === player.index) {
+          cars[index - 1 ].shapeColor = "red";
+          camera.position.x = displayWidth / 2
+          camera.position.y = cars[index - 1].y;
+        }
+        else {
+          cars[index - 1].shapeColor = "black";
+        }
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance += 20;
+      player.update();
+    }
+    drawSprites();
+  }
+}
